4eb39f927893373f2c95ece10ed34f699185076d
Gstars01 <khw4986@gmail.com>
2022-05-31 10:27:01 +0900

    Initial commit

--- day053102.c
+++ day053102.c
@@ -1,19 +1,15 @@
-//
-// Created by khw49 on 2022-05-31.
-//
-#include <stdio.h>
-#include <string.h>
-#include <malloc.h>
-typedef struct SHuman{
+#include <Stdio.h>
+
+struct SHuman{
     char name[12];
     int age;
     double height;
-}Human;
+};
 int main(){
-    Human *pJuso[5];
-    for(int i=0;i<5;i++){
-        pJuso[i] = (Human *) malloc(sizeof (Human));
-    }
+    struct SHuman Kim = {"±è»óÇü"  ,29,181.4};
+    struct SHuman lee;
 
+    lee = Kim;
+    printf("%s : %d¼¼, Å° %.2f\n",lee.name,lee.age,lee.height);
     return 0;
 }
